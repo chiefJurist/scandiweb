ee4b0639d9377d5cf3cc3fca85ea43d9a6715b1b
Anthony Nnanna <anthonynnanna004@gmail.com>
2023-05-29 08:04:58 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+    // Connect to the database
+    $servername = 'localhost';
+    $username = 'chiefJurist';
+    $password = '#Chibueze2003';
+    $dbname = 'scandiweb';
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //Checking the connection
+    if (!$conn) {
+        echo "connection error" . mysqli_connect_error();
+    }
+?>
